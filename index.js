9894ce37d380f5442ccd2213383a9f95a307cb1e
Alexey Astafev <efsneiron@gmail.com>
2019-11-01 10:47:40 +0300

    added new files

---
+++ index.js
@@ -0,0 +1,9 @@
+let container = document.getElementById('container');
+
+const inc = () => {
+    container.innerHTML = +container.innerHTML + 1
+};
+
+const dec = () => {
+    container.innerHTML = +container.innerHTML - 1
+};
